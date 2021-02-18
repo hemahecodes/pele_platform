d8516a09334ac172d7e08f049051046929cfad47
hemahecodes <helena.martin@alum.esci.upf.edu>
2021-02-18 08:18:44 +0100

    PELE-432; sdf to pdb bug

--- pele_platform/Frag/libraries.py
+++ pele_platform/Frag/libraries.py
@@ -1,12 +1,66 @@
 import glob
 import os
 import subprocess
-
+import sys
 from pele_platform.constants import constants as cs
 import pele_platform.Utilities.Helpers.helpers as hp
+from rdkit import Chem
+from rdkit.Chem import rdmolfiles
+from rdkit.Chem.AtomPairs import Pairs
+from itertools import combinations
+from rdkit.Chem import AllChem
+import tempfile
 
 OUTPUT = "input.conf"
 
+'''
+def growing_sites_symmetry(fragment,user_bond):
+    bonds = []
+    mol = Chem.MolFromPDBFile(fragment, removeHs=False)
+    #mol2 = Chem.MolToSmiles(mol)
+    symmetryList = []
+    counter = 0
+    for atom in mol.GetAtoms():
+        fing = AllChem.GetMorganFingerprint(atom,2)
+        print(fing)
+        previouscounter = counter
+        idx = atom.GetIdx()
+        neighborList = [neighbor.GetIdx() for neighbor in atom.GetNeighbors()]
+        if len(neighborList) < 2:
+            continue
+        neighborCombinations = list(combinations(neighborList,2))
+        for neighborSet in neighborCombinations:
+            NewMol = Chem.RWMol(mol) 
+            NewMol.RemoveBond(neighborSet[0],idx)
+            NewMol.GetAtomWithIdx(neighborSet[0]).SetNoImplicit(True)
+            NewMol.RemoveBond(neighborSet[1],idx)
+            NewMol.GetAtomWithIdx(neighborSet[1]).SetNoImplicit(True)
+            NewMol.GetAtomWithIdx(idx).SetNoImplicit(True)
+            Chem.SanitizeMol(NewMol)
+            Fragmented_Smiles = str(Chem.MolToSmiles(NewMol))
+            FragA, FragB, FragC = sorted(Fragmented_Smiles.split('.'))
+            if (FragA == FragB) or (FragA == FragC ) or (FragB == FragC):
+                if previouscounter == counter: 
+                    symmetryList.append([idx])
+                    symmetryList[counter].append(neighborSet)
+                    counter += 1
+                else:
+                    symmetryList[counter - 1].append(neighborSet)
+    print(symmetryList)
+    sys.exit()
+    if mol:
+        heavy_atoms = [a for a in mol.GetAtoms() if a.GetSymbol() != "H"]
+        for a in heavy_atoms:
+            hydrogens = [n for n in a.GetNeighbors() if n.GetSymbol() == "H" and n.GetIdx() in number]
+            at_name = a.GetMonomerInfo().GetName().strip()
+            for h in hydrogens:
+                if h not in matches:
+                    h_name = h.GetMonomerInfo().GetName().strip()
+                    bonds.append("{} {} {}-{}".format(fragment, user_bond, at_name, h_name))
+    return bonds
+    
+'''
+    
 def growing_sites(fragment, user_bond):
     """
     Retrieves all possible growing sites (hydrogens) on the fragment. Takes PDB fragment file as input.
@@ -30,10 +84,13 @@ def growing_sites(fragment, user_bond):
     return bonds
 
 
+def temp_opener(name, flag, mode=0o777):
+    return os.open(name, os.O_TEMPORARY, mode)
+
 def sdf_to_pdb(file_list, path, logger):
 
     out = []
-
+    fo = tempfile.NamedTemporaryFile()
     if file_list:
         converted_mae = []
         output = []
@@ -42,19 +99,22 @@ def sdf_to_pdb(file_list, path, logger):
         schrodinger_path = os.path.join(cs.SCHRODINGER, "utilities/structconvert")
         command_mae = "{} -isd {} -omae {}"
         command_pdb = "{} -imae {} -opdb {}"
-
+        #with tempfile.TemporaryFile(mode = 'w+') as fp:
         for f in file_list:
             fout = os.path.splitext(os.path.basename(f))[0] + ".mae"
             fout_path = os.path.join(os.path.dirname(f), fout)
             try:
                 command_mae = command_mae.format(schrodinger_path, f, fout_path)
+                fp = tempfile.TemporaryFile()
+                #subprocess.Popen(command_mae.split(), stdout=fp, stderr=subprocess.STDOUT)
                 subprocess.call(command_mae.split())
-                converted_mae.append(fout_path)
+                converted_mae.append(fp)
             except Exception as e:
                 logger.info("Error occured while converting SD files to mae.", e)
-        
+         
         # convert all MAE to PDB, it will result in a lot of numbered pdb files
         for c in converted_mae:
+            
             fout = c.replace(".mae",".pdb")
             try:
                 command_pdb = command_pdb.format(schrodinger_path, c, fout)
@@ -62,13 +122,15 @@ def sdf_to_pdb(file_list, path, logger):
                 os.remove(c)
             except Exception as e:
                 logger.info("Error occured while converting mae to PDB.", e)
-        
+            
         pdb_pattern = os.path.splitext(converted_mae[0])
         converted_pdb = glob.glob(pdb_pattern[0]+"*"+".pdb")
-        
+        sys.exit()        
         # ~~~ If it's stupid but it works (?), it isn't stupid. ~~~
         
         # read in PDB file created by Schrodinger, substitute residue name and add chain ID
+        out = []
+        fp = tempfile.TemporaryFile()
         for c in converted_pdb:
             with open(c, "r") as fin:
                 lines = fin.readlines()
@@ -83,7 +145,11 @@ def sdf_to_pdb(file_list, path, logger):
             with open(c, "w") as fout:
                 for line in new_lines:
                     fout.write(line)
-        out =  converted_pdb
+                for line in fout.readlines():
+                    fp.write(line.encode()) 
+            out.append(fp)
+            os.remove(c)
+        #out =  converted_pdb
     return out
 
 def get_library(frag_library):
